75c6311125ab8ce40093aa60c89007faf232f06e
stefman <sha32768@gmail.com>
2017-01-17 22:59:27 +0000

    add prototype

--- src/greetr.js
+++ src/greetr.js
@@ -1,5 +1,5 @@
 (function(global, $){
-    
+
     var Greetr = function(firstname, lastname){
         return new Greetr.init(firstname, lastname);
     };
@@ -8,5 +8,15 @@
         this.firstname = firstname;
         this.lastname = lastname;
     };
+
+    Greetr.prototype = {
+        greet: function(){
+            return this.firstname + " " + this.lastname;
+        }
+    }
+
+    Greetr.init.prototype = Greetr.prototype;
+
+    global.Greetr = Greetr;
 
 })(window, jQuery);
